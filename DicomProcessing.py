a5328ca7e6e49529bad54a477764b21c2ea2186c
Rajkoumar <sylvain.rajkoumar@gmail.com>
2018-11-19 13:53:44 +0100

    changes in segmentation

--- DicomProcessing.py
+++ DicomProcessing.py
@@ -8,10 +8,12 @@ class DicomProcessing(object):
         self.segmentation_threshold = 0
 
     def segmentation(self, image):
-        ret, segmented_image = cv2.threshold(image, self.segmentation_threshold, 65535, cv2.THRESH_BINARY)
+        # Voir adaptativeThresold à la place
+        ret, mask = cv2.threshold(image, self.segmentation_threshold, 65535, cv2.THRESH_BINARY)
         ellipsekernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
-        segmented_image = cv2.erode(segmented_image, ellipsekernel, iterations = 1)
-        segmented_image = cv2.dilate(segmented_image, ellipsekernel, iterations = 1)
+        mask = cv2.erode(mask, ellipsekernel, iterations = 1)
+        mask = cv2.dilate(mask, ellipsekernel, iterations = 1)
+        segmented_image = cv2.bitwise_and(image, mask)
         return segmented_image
 
     def set_segmentation_threshold(self, value):
